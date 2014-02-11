55dc8680728a628b1c055f3446a6b71e0a03336b
mitzisong <mitzisong@gmail.com>
2014-02-10 17:39:13 -0800

    List is sorted by frequency.

--- wordcount.py
+++ wordcount.py
@@ -7,7 +7,6 @@ test = open(textfile)
 # TO-DO: Check if you really need this
 readfile = test.read()
 
-# Separate readfile string into a list
 # Use split function to separate words followed by a space
 # Convert all letters to lowercase 
 # Use strip function to strip punctuation
@@ -16,16 +15,11 @@ readfile = test.read()
 # Formatting characters to lower case, split to list, and remove puncutation
 readfile = readfile.lower()
 dash_word_string = readfile.replace("--"," ")
+# Separate readfile string into a list
 underscore_word_string = dash_word_string.replace("_", " ")
 word_list = underscore_word_string.split()
 
-# TO-DO Ask instructors if there is a better way? (twain.txt is messy, lots of punct)
-# TO-DO Check to see if punct is grouped together in ascii table, google comprehensive punct pack?
-
-# format_list = []
-
-# build a list using string.punctuation
-# create a loop to strip punctuation from file
+test.close()
 
 
 # TO DO ask if it's removing ---- --hello-- because of looping? 
@@ -35,27 +29,27 @@ for word in word_list:
     stripped_new_word = word.strip(string.punctuation)
     format_list.append(stripped_new_word)
 
-"""
-TO-DO ask if there is a way to do this without nested for loop
-punctuation = [",", '.', '[', ']', '"', '(', ')', ':', ';', '?', '!', "'"]
-
-for word in word_list:
-    new_word = 
-
-print format_list
-"""
 
 # If item is not in dictionary, create a new key entry. If not, add 1 to the existing entry
 word_dict = {}
 # TO DO ask if better way than nested for loops
 # iterate through list and check against dictionary to see if item in there, if not create new, if so, add one
 for word in format_list:
-        if word_dict.get(word) != None:
-            word_dict[word] += 1
-        else: 
-            word_dict[word] = 1
+    word_dict[word] = word_dict.get(word, 0) + 1
 
+
+rev_dict = {}
 for key, value in word_dict.iteritems():
-    print str(key) + ' ' + str(value)
+    if rev_dict.get(value) != None: #if it sin there, append to the list of the value in rev_dict
+        rev_dict[value].append(key)
+    else: # if not in there, create a new key in rev_dict
+        rev_dict[value] = [key]
 
-test.close()
+    # if new_dict.get(value) != None:
+    #     new_dict[value] = new_value_list.append(word)
+    # else: 
+    #     new_dict[value] = key
+
+desc_ord_list = sorted(rev_dict.keys(), reverse = True)
+for item in desc_ord_list:
+    print "%s %s" % (item, rev_dict[item])
